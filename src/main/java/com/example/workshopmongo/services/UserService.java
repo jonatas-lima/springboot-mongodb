9972be47e148d738b554d6c935cf02a2b7dae1aa
Jonatas Ferreira <ferreiradelimajonatas@gmail.com>
2020-07-24 10:42:26 -0300

    método update para atualização do usuário

--- src/main/java/com/example/workshopmongo/services/UserService.java
+++ src/main/java/com/example/workshopmongo/services/UserService.java
@@ -29,8 +29,24 @@ public class UserService {
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}
+
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+	
+	public User update(User obj) {
+		User objAux = findById(obj.getId());
+		updateData(objAux, obj);
+		return repo.save(objAux);
+	}
 	
 	public User fromDTO(UserDTO objDTO) {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
+	}
+	
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
 	}
 }
